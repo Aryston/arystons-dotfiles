c7877d27ec265989247f0bcc60f01712cac1a309
Igor Jagec <45634513+Aryston@users.noreply.github.com>
2024-07-11 17:12:03 +0100

    Added function Get-SystemInfo

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -21,3 +21,39 @@ function Get-Uptime {
 
 # Create an alias 'uptime' for the Get-Uptime function
 Set-Alias -Name uptime -Value Get-Uptime
+
+function Get-SystemInfo {
+    param (
+        [string]$ComputerName = $null
+    )
+
+    # ScriptBlock to retrieve system information
+    $sysInfoScriptBlock = {
+        $osInfo = systeminfo | Select-String -Pattern "OS Name", "OS Version"
+        $biosInfo = systeminfo | Select-String -Pattern "BIOS Version"
+        $hardwareInfo = Get-WmiObject win32_systemenclosure | Select-Object Manufacturer, SerialNumber
+
+        # Construct output as a string
+        $output = @(
+            $osInfo[0].Line
+            $osInfo[1].Line
+            $biosInfo.Line
+            "Manufacturer: $($hardwareInfo.Manufacturer)"
+            "SerialNumber: $($hardwareInfo.SerialNumber)"
+        )
+        $output -join "`n"
+    }
+
+    # Run the script block locally or remotely based on $ComputerName
+    if ($ComputerName) {
+        $result = Invoke-Command -ComputerName $ComputerName -ScriptBlock $sysInfoScriptBlock
+    } else {
+        $result = Invoke-Command -ScriptBlock $sysInfoScriptBlock
+    }
+
+    # Display the result
+    Write-Output $result
+}
+
+# Save the function in your profile
+Set-Alias -Name Get-SysInfo -Value Get-SystemInfo
